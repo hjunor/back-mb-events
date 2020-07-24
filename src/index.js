037a688d7c1930f6c48e8213f29ad35a731f342a
hjunor <hjunor@gmail.com>
2020-07-23 21:16:59 -0300

    add: dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+require('./services/database')
 const express = require('express')
 const routes = require('./routes')
 const app = express()
+
 app.use(express.json())
 app.use(routes)
 
