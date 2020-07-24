f611b89b20939748f73d26ba7e6228dffd257807
hjunor <hjunor@gmail.com>
2020-07-24 16:51:54 -0300

    add: modells event

---
+++ src/models/Event.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const uuid = require('uuid')
+const EventSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuid.v4()
+    },
+    file: {
+      type: String
+    },
+    title: {
+      type: String,
+      required: true,
+
+    },
+    types: {
+      type: String,
+      ref: 'Types',
+      required: true,
+    },
+    category: {
+      type: String,
+      ref: 'Category',
+      required: true,
+    },
+    tickets: {
+      type: String,
+      required: true,
+
+    },
+    adress: {
+      type: Object,
+      required: true,
+
+    },
+    description: {
+      type: String,
+      required: true,
+
+    },
+    price: {
+      type: String,
+      required: true,
+
+    },
+    id_user: {
+      type: String,
+      ref: 'Users',
+      required: true
+    },
+    users: [{
+      type: String,
+      ref: 'Users'
+    }],
+    date: {
+      type: String,
+      required: true
+    },
+    hours: {
+      type: String,
+      required: true
+    }
+
+
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Event', EventSchema);
