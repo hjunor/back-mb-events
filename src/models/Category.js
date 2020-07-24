8ee61ead4fc1a3bbd722901c01430ecb02b1b4f6
hjunor <hjunor@gmail.com>
2020-07-24 16:53:12 -0300

    add: Schema Category

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-
+const uuid = require('uuid')
 const CategoryShema = new mongoose.Schema(
   {
     _id: {
       type: String,
-      default: true,
+      default: uuid.v4()
     },
     title: {
       required: true,
       type: String,
+      unique: true,
     }
   },
   { timestamps: false }
