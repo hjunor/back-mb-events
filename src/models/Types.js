9ae4f8f616890f200fe02836fe4dfb353bbc421f
hjunor <hjunor@gmail.com>
2020-07-24 16:52:53 -0300

    add: Schema Types

--- src/models/Types.js
+++ src/models/Types.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
-
+const uuid = require('uuid');
 const TypesSchema = new mongoose.Schema(
   {
     _id: {
       type: String,
-      default: true,
+      default: uuid.v4(),
     },
     title: {
-      required: true,
       type: String,
+      required: true,
+      unique: true,
     }
   },
   { timestamps: false }
 );
+
 
 module.exports = mongoose.model('Types', TypesSchema);
