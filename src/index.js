ae682cf49f78cbc13cfc68db473e9df0d182f05b
hjunor <hjunor@gmail.com>
2020-07-24 20:05:21 -0300

    add: method express static

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require('./services/database')
 const express = require('express')
 const routes = require('./routes')
 const app = express()
+const path = require('path')
+
 
 app.use(express.json())
 app.use(routes)
-
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333, (error) => {
   try {
     console.log('sever starter')
