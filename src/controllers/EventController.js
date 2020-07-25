951b4e1517f24e799e5dc30af6aa1e96e5b26ee6
hjunor <hjunor@gmail.com>
2020-07-25 01:32:11 -0300

    add: update delete storeUser indexUser

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -35,12 +35,94 @@ class EventController {
     })
     return response.json(event)
   }
-  index(request, response) {
-    const { id } = request.query
+  async update(request, response) {
+    const {
+      tickets,
+      title,
+      price,
+      types,
+      category,
+      street,
+      neighborhood,
+      city,
+      number,
+      state,
+      id_user,
+      description,
+      date,
+      hours } = request.body
+    const file = request.file.filename
+    const { id } = request.params
+    const user = await UserShema.findById(id_user)
 
-    response.json({ ok: id })
+    if (!user || user.admin === false) {
+      return response.status(400).json({ error: 'no authorizade' });
+    }
+
+    const event = await EventShema.findByIdAndUpdate(id, {
+      file,
+      tickets,
+      title,
+      price,
+      types,
+      category,
+      adress: {
+        street,
+        number,
+        neighborhood,
+        city,
+        state
+      },
+      description,
+      date,
+      hours,
+      id_user,
+    }, { new: true })
+    return response.json(event)
   }
+  async index(request, response) {
+    const event = await EventShema.find();
 
+    response.json(event)
+  }
+  async delete(request, response) {
+    const { id, id_user } = request.params
+    const user = await UserShema.findById(id_user)
+
+    if (!user || user.admin === false) {
+      return response.status(400).json({ error: 'no authorizade' });
+    }
+    const event = await EventShema.findByIdAndDelete({
+      _id: id
+    })
+
+    return response.json(event)
+  }
+  async storeUser(request, response) {
+    const { id } = request.params
+    const { id_user } = request.body
+    const user = await EventShema.findById(id)
+    if (!user) {
+      return response.status(400).json({ error: 'no authorizade' });
+    }
+    const arry = [...user.users, id_user]
+    const event = await EventShema.findByIdAndUpdate(id, {
+      users: arry
+    }, { new: true })
+
+    return response.json(event)
+
+  }
+  async indexUser(request, response) {
+    const { id } = request.params
+    const user = await UserShema.findById(id)
+    if (!user) {
+      return response.status(400).json({ error: 'no authorizade' });
+    }
+    const event = await EventShema.find({ users: id });
+
+    return response.json(event)
+  }
 }
 
 module.exports = new EventController()
