e485fd1a02848a2153377693cce323d3f71929fa
hjunor <hjunor@gmail.com>
2020-07-25 01:35:11 -0300

    refactor: refactor index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const routes = require('./routes')
 const app = express()
 const path = require('path')
 
-
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
