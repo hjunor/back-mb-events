50357669a335b76cd82d0b6e712916352117b90e
hjunor <hjunor@gmail.com>
2020-07-29 10:37:18 -0300

    refactor: index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 app.use(express.json())
 app.use(routes)
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.listen(3333, (error) => {
   try {
     console.log('sever starter')
