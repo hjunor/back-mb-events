e350b4e13c5392f304ca8b48259476aff0d9e907
hjunor <hjunor@gmail.com>
2022-06-21 12:20:02 -0300

    refactor: class MongoDb use actions contructor

--- src/services/db/mongo.js
+++ src/services/db/mongo.js
@@ -1,33 +1,32 @@
 // const ICrud = require("./base/interfaceDb");
 const Mongoose = require("mongoose");
-const STATUS = {
-  0: "Disconectado",
-  1: "Conectado",
-  2: "Conectando",
-  3: "Disconectando",
-};
-
-const URL = process.env.URL;
-
-const option = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-};
 
 class MongoDB {
   constructor() {
     this._driver = Mongoose.connection;
+    this._url = process.env.URL;
+    this._option = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    };
+
+    this._status = {
+      0: "Disconectado",
+      1: "Conectado",
+      2: "Conectando",
+      3: "Disconectando",
+    };
   }
   async isConnected() {
-    const state = STATUS[Mongoose.connection.readyState];
+    const state = this._status[Mongoose.connection.readyState];
 
     console.log(state);
   }
 
   connect() {
-    Mongoose.connect(URL, option, (erro) => {
+    Mongoose.connect(this._url, this._option, (erro) => {
       !erro ? this.isConnected() : console.log(erro);
     });
   }
